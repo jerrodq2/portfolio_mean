2d8df7835baaf44fa79ad2ca05bfec2310ed3fdb
Jerrod Quintana <jerrodq2@yahoo.com>
2017-01-03 18:24:39 -0600

    created main pages, implemented nabber and background, added bootstrap, installed ejs in node to deal with pages besides partials

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,6 +4,10 @@ var Main = require('../serverControllers/main.js')
 
 module.exports = function(app){
   app.get('/', Main.main)
+  app.get('/contact', Main.contact)
+  app.get('/algorithms', Main.algorithms)
+  app.get('/projects', Main.projects)
+
 }
 
 // *******************End*******************
